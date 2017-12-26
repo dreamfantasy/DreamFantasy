ad9fc38002cb6fe2346b8d07819e55f1e2ae8294
yamada <a>
2017-12-27 01:22:29 +0900

    タイトル画面

---
+++ Assets/Resources/Script/Common/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	const float WIDTH  =  9.0f;
+	const float HEIGHT = 16.0f;
+
+	void Awake( ) {
+		GetComponent< Camera >( ).rect = getRect( ); 
+	}
+
+	Rect getRect( ) {
+		Rect rect = new Rect( );
+		float target_aspect = WIDTH / HEIGHT;
+		float screen_aspect = ( float )Screen.width / Screen.height;
+		float scale_height = screen_aspect / target_aspect;
+
+		if ( 1.0f > scale_height ) {
+			rect.x = 0;
+			rect.y = ( 1.0f - scale_height ) / 2.0f;
+			rect.width = 1.0f;
+			rect.height = scale_height;
+		} else {
+			float scale_width = 1.0f / scale_height;
+			rect.x = ( 1.0f - scale_width ) / 2.0f;
+			rect.y = 0.0f;
+			rect.width = scale_width;
+			rect.height = 1.0f;
+		}
+		return rect;
+	}
+}
